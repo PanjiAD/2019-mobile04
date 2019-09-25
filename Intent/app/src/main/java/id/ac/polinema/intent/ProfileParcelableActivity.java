546b2ebb2c98bf0eb010ad280f9579f359c6988e
panjiad <panjiawwaludi430@gmail.com>
2019-09-25 21:36:27 +0700

    parcelable not fix

--- Intent/app/src/main/java/id/ac/polinema/intent/ProfileParcelableActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ProfileParcelableActivity.java
@@ -3,20 +3,32 @@ package id.ac.polinema.intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import id.ac.polinema.intent.Model.User;
 
 public class ProfileParcelableActivity extends AppCompatActivity {
+
+    private TextView textUsername;
+    private TextView textName;
+    private TextView textAge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_parcelable);
-
+        textUsername = findViewById(R.id.text_username);
+        textName = findViewById(R.id.text_name);
+        textAge = findViewById(R.id.text_age);
         // TODO: bind here
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
-
             // TODO: display value here
+            User user = extras.getParcelable(ParcelableActivity.USER_KEY);
+            textUsername.setText(user.getUsername());
+            textName.setText(user.getName());
+            textAge.setText(String.valueOf(user.getAge()));
         }
     }
 }
